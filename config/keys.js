531f56d743e5933d3f6d1875978293ef4d978ddd
Kristian Charbonneau <kristiancharbonneau@Kristians-MBP.home>
2018-07-03 14:14:06 -0400

    setting up models and api

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://kristian:kris123@ds125031.mlab.com:25031/mern_proj'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router()
+
+const Item = require('../../models/Item')
+
+// GET ALL ITEMS
+router.get('/', (req, res) => {
+
+})
+
+module.exports = router;
